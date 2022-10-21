9ab78ea6d9f7b0bc1046f6745a2acbb062c72b28
nomandhoni-cs <nomandhoni@yahoo.com>
2022-10-21 18:38:36 +0600

    Little ui fixed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
     console.log(props);
+    const {img, name} = props.product;
     return (
-        <div>
-            <h3>Hello{props.product.name}</h3>
-        </div>
+<div className="product-container">
+    <div className="product-image">
+        <img src={img} alt="" />
+    </div>
+    <div className="product-info">
+        <h4>{name}</h4>
+    </div>
+</div>
     );
 };
 
